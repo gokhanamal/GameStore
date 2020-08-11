5a68b138e7c9db8d68d61acb5cdcf2823bfb6aaf
Gokhan Namal <gokhanamal@gmail.com>
2020-08-11 15:17:58 +0300

    Initial Commit

---
+++ GameStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameStore
+//
+//  Created by Gokhan Namal on 11.08.2020.
+//  Copyright © 2020 Gokhan Namal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
